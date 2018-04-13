b9df889ba3a3020a044b3f8c5ab1dca9a4865886
programmer-kapoor <shubhamkapoor9798@gmail.com>
2018-04-13 15:41:20 +0530

    twin prime

--- greeksOfgreek/twinNum.cpp
+++ greeksOfgreek/twinNum.cpp
@@ -4,10 +4,10 @@ using namespace std;
 // Function prototype
 
 
-
+bool twinFun(int j);
 int main()
 {
-    int a,test;
+    int a,test,j,i;
     cout<<"number of test case";
     cin>>test;
     int temp1=0;
@@ -17,13 +17,29 @@ int main()
         cout<<"enter the range for twin prime";
         cin>>a;
 
-        for(int i=2;i<=a;i++){
-                for(int j=2;j<i;j++){
+        for(i=2;i<=a;i++){//range
+                bool prime = true;
+                for( j=2;j<i;j++){//check
+
+                   if(a==1)
+                     cout<<"not a prime number";
+
+                   if(i%j==0)
+                    prime=false;
+
+
 
                 }
+               if(j==2)
+                continue;
 
-
-
+            if((prime==true)&&(j+2<=a))
+                    {
+                      // cout<<j<<endl;
+                        int k=j+2;
+                        if(twinFun(k))
+                          cout<<"("<<j<<","<<k<<")"<<endl;
+                    }
         }
 
     }
@@ -32,7 +48,18 @@ int main()
     return 0;
 }
 
+bool twinFun(int t)//9
+{
+    bool prime=true;
+           for( int j=2;j<t;j++){//check //j=2;j<9;j++
+              // cout<<t;
+                  if(t%j==0)
+                     prime=false;
 
+                }
+        return prime;
+
+}
 
 
 
