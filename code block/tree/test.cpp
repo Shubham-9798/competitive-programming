d412dab1b5fa331a89448215330d9d759cee2540
programmer-kapoor <shubhamkapoor9798@gmail.com>
2018-06-10 00:26:43 -0700

    tree

--- code block/tree/test.cpp
+++ code block/tree/test.cpp
@@ -1,4 +1,7 @@
 #include<stdio.h>
+#include <stdlib.h>
+#include<iostream>
+using namespace std;
 
 
 //node created
@@ -9,8 +12,48 @@ struct node
   struct node *right;
 };
 
+struct node* newNode(int data){
+
+     // Allocate memory for new node
+  struct node* node = (struct node*)malloc(sizeof(struct node));
+
+   node->data=data;
+
+   node->left=NULL;
+   node->right=NULL;
+
+
+   return node;
+}
 int main(){
-struct node t;
+struct node *root=newNode(1);
+
+   root->left        = newNode(2);
+   root->right       = newNode(3);
+
+
+     /* 2 and 3 become left and right children of 1
+           1
+         /   \
+        2      3
+     /    \    /  \
+    NULL NULL NULL NULL
+  */
+
+
+  root->left->left  = newNode(4);
+  /* 4 becomes left child of 2
+           1
+       /       \
+      2          3
+    /   \       /  \
+   4    NULL  NULL  NULL
+  /  \
+NULL NULL
+*/
+
+cout<<root->right->data;
+
 
 
 return 0;
