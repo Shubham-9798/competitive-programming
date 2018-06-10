41c4d28ac8dded7a2902d2d4e3ffc72a16d6d97b
programmer-kapoor <shubhamkapoor9798@gmail.com>
2018-06-09 23:19:01 -0700

    stack

---
+++ code block/tree/test.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+
+//node created
+struct node
+{ //can't init member here
+  int data;
+  struct node *left;
+  struct node *right;
+};
+
+int main(){
+struct node t;
+
+
+return 0;
+}
